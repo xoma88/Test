d834e021cb4aa65d457592b3d0700b6e9f787e05
xoma88 <homich8897@gmail.com>
2021-03-22 23:28:31 +0200

    finish

---
+++ app/src/main/java/com/example/test/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.test
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.test.ui.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private val runnable = Runnable {
+        if (!isFinishing) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+}
